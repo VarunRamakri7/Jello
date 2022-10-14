fc09ca318d068a4a7a096bb99c7bbb7e1a6f5152
Varun Ramakrishnan <oceans.varun@gmail.com>
2022-10-14 17:56:08 -0400

    Removed blinn-phong shading

--- Jello/jello_fs.glsl
+++ Jello/jello_fs.glsl
@@ -19,32 +19,11 @@ layout(location = 2) uniform float time;
 in vec2 tex_coord;
 in vec3 position;
 in vec3 normal;
+in vec3 light_intensity;
 
 out vec4 fragcolor; //the output color for this fragment    
 
-// Calculate blinn-phong shading
-vec3 blinnPhong(vec3 pos, vec3 n)
-{  
-	vec3 ambient = Light.La.xyz * Material.Ka.xyz;
-	vec3 s = normalize(Light.light_w.xyz - pos);
-	
-	float sDotN = max(dot(s, n), 0.0f);
-	vec3 diffuse = Material.Kd.xyz * sDotN;
-	vec3 spec = vec3(0.0f);
-	
-	if(sDotN > 0.0f)
-	{
-	  vec3 v = normalize(-pos);
-	  vec3 h = normalize(v + s);
-	  spec = Material.Ks.xyz * pow(max(dot(h,n), 0.0), Material.Shininess);
-	}
-	
-	return ambient + Light.Ld.xyz + Light.Ls.xyz * (diffuse + spec);
-}
-
 void main(void)
 {   
-    fragcolor = vec4(blinnPhong(position, normal), 1.0f);
-
-	//fragcolor = vec4(tex_coord, 0.0f, 1.0f); // Color as texture coordinates
+	fragcolor = vec4(normal, 1.0f); // Phong shading
 }
