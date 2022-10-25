7eaf812f3f2e09158d09ddbc9d2a897bc2807e64
Varun Ramakrishnan <oceans.varun@gmail.com>
2022-10-25 18:45:09 -0400

    Added texture uniform

--- Jello/template_vs.glsl
+++ Jello/template_vs.glsl
@@ -5,6 +5,8 @@ layout(location = 1) uniform mat4 PV;
 layout(location = 2) uniform float time;
 layout(location = 3) uniform int pass;
 
+uniform sampler2D fboTex;
+
 layout(std140, binding = 0) uniform LightUniforms {
 	vec4 light_w; // world-space light position
 	vec4 bg_color; // Background color
@@ -26,17 +28,23 @@ out vec3 normal;
 out vec3 position;
 out vec3 eye_dir;
 out vec3 light_dir;
+out float depth;
+
+const float near = 0.1f;
+const float far = 100.0f;
 
 void main(void)
 {
 	// Assign position depending on pass
 	position = (pass == 0) ? pos_attrib : vec3(M * vec4(pos_attrib, 1.0)); // World-space vertex position
-	tex_coord = tex_coord_attrib; //send tex_coord to fragment shader
+	tex_coord = tex_coord_attrib; // Send tex_coord to fragment shader
 	
 	eye_dir = -1.0 * normalize(vec3(M * vec4(pos_attrib, 1.0)));
 	light_dir = normalize(Light.light_w).xyz;
 
 	normal = normalize(M * vec4(normal_attrib, 0.0)).xyz;
+
+	//depth = PV * vec4(pos_attrib, 1.0).z; // Get eye-space depth
 
 	gl_Position = (pass == 0) ? vec4(position, 1.0) : PV * vec4(position, 1.0);
 }
